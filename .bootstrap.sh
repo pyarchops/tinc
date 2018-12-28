8ada51f4697db079eb6f32ac84035b0eb51d8c80
Azulinho <mail@azulinho.com>
2018-12-28 22:21:16 +0000

    drop .pyenv

--- .bootstrap.sh
+++ .bootstrap.sh
@@ -7,30 +7,6 @@ normal=$(tput sgr0)
 
 echo "${bold}running bootstrap ....${normal}"
 
-test -e .pyenv/bin && export PATH=./pyenv/bin:$PATH
-env
-
-if ! `which pyenv >/dev/null 2>&1`; then
-    echo "${bold}installing pyenv....${normal}"
-    test -e .pyenv || git clone https://github.com/pyenv/pyenv.git .pyenv
-fi
-
-echo "${bold}activating  pyenv....${normal}"
-test -e .pyenv && PATH=./pyenv/bin:$PATH ; export PATH
-test -e .pyenv && PYENV_ROOT="$HOME/.pyenv"; export PYENV_ROOT
-env
-eval "$(pyenv init)"
-env
-echo "${bold}looking for a new pyenv version....${normal}"
-pyenv update
-
-local_python_version=`cat .python-version`
-
-if ! `pyenv versions | grep ${local_python_version} >/dev/null 2>&1`; then
-    echo "${bold}installing python version ${local_python_version} using pyenv${normal}"
-    pyenv install ${local_python_version}
-fi
-
 if [ ! -e .venv ]; then
     echo "${bold}initializing virtualenv...${normal}"
     virtualenv --python=`which python` .venv
