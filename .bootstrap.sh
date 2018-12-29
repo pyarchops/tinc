156ad07a4fd77a168eaf8e7c42da208cef57f87e
Azulinho <mail@azulinho.com>
2018-12-29 00:19:34 +0000

    simplifies gitlab ci setup

--- .bootstrap.sh
+++ .bootstrap.sh
@@ -7,27 +7,6 @@ normal=$(tput sgr0)
 
 echo "${bold}running bootstrap ....${normal}"
 
-
-if ! `which pyenv >/dev/null 2>&1`; then
-    echo "${bold}installing pyenv....${normal}"
-    curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
-    echo 'export PATH="~/.pyenv/bin:$PATH"' >> ~/.profile
-    echo 'eval "$(pyenv init -)"' >> ~/.profile
-fi
-
-echo "${bold}activating  pyenv....${normal}"
-test -e ~/.profile && . ~/.profile
-
-echo "${bold}looking for a new pyenv version....${normal}"
-pyenv update
-
-local_python_version=`cat .python-version`
-
-if ! `pyenv versions | grep ${local_python_version} >/dev/null 2>&1`; then
-    echo "${bold}installing python version ${local_python_version} using pyenv${normal}"
-    pyenv install ${local_python_version}
-fi
-
 if [ ! -e .venv ]; then
     echo "${bold}initializing virtualenv...${normal}"
     virtualenv --python=`which python` .venv
